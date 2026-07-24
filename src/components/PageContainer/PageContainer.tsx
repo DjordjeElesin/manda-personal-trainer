a49fd3a9471e98ec1d428739d7f7fa19d437d75f
DjordjeElesin <125301297+DjordjeElesin@users.noreply.github.com>
2026-07-24 13:32:30 +0200

    fix: rewrite unknown paths to index.html (#4)
    * fix: rewrite unknown paths to index.html
    
    * chore: clean up unused component

--- src/components/PageContainer/PageContainer.tsx
+++
@@ -1,7 +0,0 @@
-export const PageContainer = () => {
-  return (
-    <div className="w-full relative h-screen">
-      <div className="h-full scroll-auto flex flex-col"></div>
-    </div>
-  );
-};
